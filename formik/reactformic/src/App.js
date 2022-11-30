569f8c8033ced8e3982226e9b87eabb85178c008
ibrahim ibrahimli <89877445+ibrahimibrahimli@users.noreply.github.com>
2022-11-30 18:53:12 +0400

    Add files via upload

---
+++ formik/reactformic/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ValidationSchemaExample from './FormValidation'
+
+const App = () => {
+  return (
+    <div>
+      <ValidationSchemaExample />
+    </div>
+  )
+}
+
+export default App
